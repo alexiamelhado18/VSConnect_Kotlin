110d497769099a25c6158b39bff8f630fda752ba
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2023-12-20 23:31:17 -0300

    projeto config/ tela login feita

---
+++ app/src/main/java/com/senai/vsconnect/views/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.senai.vsconnect.views
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.senai.vsconnect.R
+import java.io.IOException
+import java.io.InputStream
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
