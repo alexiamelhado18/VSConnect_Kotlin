9f873a2bad5fbc7ec2ef61fb55033d8022cb7144
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2023-12-21 00:06:50 -0300

    estruturando view llista_servico  e componente servico

---
+++ app/src/main/java/com/senai/vsconnect/views/ListaServicoActivity.kt
@@ -0,0 +1,12 @@
+package com.senai.vsconnect.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.senai.vsconnect.R
+
+class ListaServicoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_servico)
+    }
+}
